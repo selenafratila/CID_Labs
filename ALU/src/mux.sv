47923c4be7a677b6bdc75664c4a81571e3aa6602
selenafratila <selenafratila225@gmail.com>
2026-05-04 23:10:27 +0300

    Create mux.sv

---
+++ ALU/src/mux.sv
@@ -0,0 +1,17 @@
+`timescale 1ns / 1ps
+
+
+module mux(
+input logic [3:0] in0,in1,in2,in3,
+input logic [1:0] sel,
+output logic [3:0] out);
+
+always_comb
+case(sel)
+    2'b00 : out=in0;
+    2'b01 : out=in1;
+    2'b10 : out=in2;
+    2'b11 : out=in3;
+    default: out=in0;
+ endcase   
+endmodule
